8efb3aba2a05a47dc35f227eea54d4554530521e
Bogdan Ošep <bogdan.osep@gmail.com>
2025-04-10 19:59:14 +0200

    config

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,11 +1,38 @@
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_DATABASE = process.env.DB_DATABASE
+
+const DATABASE_URL = 
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` + 
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+
+  const plugins = [
+    // ...
+    {
+      resolve: "@medusajs/admin",
+      /** @type {import('@medusajs/admin').PluginOptions} */
+      options: {
+        autoRebuild: true,
+        serve: process.env.NODE_ENV === "development",
+      },
+    },
+  ]
+  
+
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  
   projectConfig: {
+    database_extra: { ssl: { rejectUnauthorized: false } },
     databaseUrl: process.env.DATABASE_URL,
+
     http: {
+      redis_url: process.env.REDIS_URL,
       redisUrl: process.env.REDIS_URL!,
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
